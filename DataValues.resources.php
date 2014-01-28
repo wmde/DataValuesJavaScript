d1a7daafe3133114c930970ca039014866003406
aude <aude.wiki@gmail.com>
2014-01-27 19:58:40 -0500

    fix resource paths

--- DataValues.resources.php
+++ DataValues.resources.php
@@ -22,8 +22,8 @@ return call_user_func( function() {
 	$remoteExtPathParts = explode( DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR , __DIR__, 2 );
 
 	$moduleTemplate = array(
-		'localBasePath' => __DIR__,
-		'remoteExtPath' => $remoteExtPathParts[1],
+		'localBasePath' => __DIR__ . '/resources',
+		'remoteExtPath' => $remoteExtPathParts[1] . '/resources',
 	);
 
 	return array(
--- js/ValueParsers.resources.php
+++ js/ValueParsers.resources.php
@@ -18,8 +18,8 @@ return call_user_func( function() {
 	$remoteExtPathParts = explode( DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR , __DIR__, 2 );
 
 	$moduleTemplate = array(
-		'localBasePath' => __DIR__ . '/src/valueParsers',
-		'remoteExtPath' => $remoteExtPathParts[1],
+		'localBasePath' => __DIR__ . '/src/ValueParsers',
+		'remoteExtPath' => $remoteExtPathParts[1] . '/src/ValueParsers',
 	);
 
 	return array(
